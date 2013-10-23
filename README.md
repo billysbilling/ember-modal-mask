4bafa6d86a103c2af29fff4f6860dee4c839a6aa
Sebastian Seilund <sebastian@seilund.com>
2013-10-23 16:30:00 -0700

    added files

---
+++ README.md
@@ -0,0 +1,1 @@
+# ember-modal-mask
---
+++ src/js/modal-mask.js
@@ -0,0 +1,46 @@
+module.exports = Em.Component.extend({
+    classNames: ['modal-mask'],
+    
+    attributeBindings: ['style'],
+
+    zIndex: 1000,
+    
+    style: function() {
+        return 'z-index:'+this.get('zIndex')+';';
+    }.property('zIndex'),
+
+    show: function() {
+        this.appendTo(this.container.lookup('application:main').get('rootElement'));
+    },
+
+    didInsertElement: function() {
+        var self = this,
+            el = this.$();
+        setTimeout(function() {
+            el.addClass('visible');
+            self.blurBelowLayer(true);
+        }, 0);
+    },
+    
+    animateDestroy: function() {
+        var el = this.$();
+        el.removeClass('visible');
+        this.blurBelowLayer(false);
+        Em.run.later(this, function() {
+            this.destroy();
+        }, 300);
+    },
+    
+    blurBelowLayer: function(blurred) {
+        var zIndex = this.get('zIndex');
+        var belowLayer = this.$().siblings('.application, .layer')
+            .filter(function(index, s) {
+                var sz = $(s).css('z-index');
+                return (sz === 'auto' || sz < zIndex);
+            })
+            .sort(function(a, b) {
+                return $(b).css('z-index') - $(a).css('z-index');
+            })[0];
+        $(belowLayer)[blurred ? 'addClass' : 'removeClass']('modal-mask-blur');
+    }
+});
